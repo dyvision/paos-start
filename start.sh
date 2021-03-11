60ebe418d8476566871e6f576d6be2ffbbb1080e
dyvision <dlang@dssinc.com>
2021-03-11 12:24:50 -0500

    bluetooth complted

--- start.sh
+++ start.sh
@@ -1,14 +1,16 @@
 sudo dhclient &
+sudo apt install expect -y
+
 
 #start update
 cd /home/parsec/paos-start/
 git reset --hard HEAD;git pull;
-sudo chmod +x *
+sudo chmod +x -R *
 
 #iot update
 cd /var/www/html/paos-iot 
 git reset --hard HEAD;git pull;
-sudo chmod +x *
+sudo chmod +x -R *
 
 #chrome launch
 google-chrome-stable --kiosk "http://parsec-arcade/start.php"
