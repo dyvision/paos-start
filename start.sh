82e9c7e4ca1aea18ea6c9ceaec5fc6c8e8f21fd5
dyvision <34869448+dyvision@users.noreply.github.com>
2021-03-10 22:54:30 -0500

    init

---
+++ start.sh
@@ -0,0 +1,19 @@
+sudo dhclient &
+
+#start update
+cd /home/parsec/paos-start/
+git reset --hard HEAD;git pull;
+sudo chmod +x *
+
+#legacy update
+cd /home/parsec/RetroPie/roms/parsec
+git reset --hard HEAD;git pull;
+sudo chmod +x *
+
+#iot update
+cd /var/www/html/paos-iot 
+git reset --hard HEAD;git pull;
+sudo chmod +x *
+
+#chrome launch
+google-chrome-stable --kiosk "http://parsec-arcade/start.php"
