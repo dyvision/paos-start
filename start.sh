5b702be5f559b58cf9b5d01fd22d78bc8ea34ea6
dyvision <dlang@dssinc.com>
2021-03-11 19:03:52 -0500

    no blanking

--- start.sh
+++ start.sh
@@ -4,6 +4,11 @@ sudo apt install expect -y
 pip3 install pynput
 export DISPLAY=:0;python3 hide-mouse.py
 
+xset s off
+xset s noblank
+xset -dpms
+xset dpms 0 0 0
+
 git clone https://github.com/atar-axis/xpadneo.git
 cd xpadneo
 sudo ./install.sh
